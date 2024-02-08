d2354b470b6f79a02dbb2eee31d7bbd0a83c1087
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-08 09:56:28 -0300

    ttesst

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/LandingPage.css';
+// import '../styles/LandingPage.css';
 
 export default function LandingPage(){
     return (
