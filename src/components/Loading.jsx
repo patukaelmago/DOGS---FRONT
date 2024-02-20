9bbff4d238b6a931263c1a59ddb47170b5f3b8ff
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-20 04:20:49 -0300

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import loading from '../styles/loading-dog.gif';
-import '../styles/Loading.css';
-import '../styles/Paginado.css';
+import loading from '../images/loading-dog.gif';
 import { useSelector } from 'react-redux';
 
 export default function Loading(){
     const allDogs = useSelector((state) => state.dogs);
 
     return (
-        <div>
-            <div >
-                <img className='loading' src={loading} alt='Loading'/>
+        <div className='flex justify-center h-screen'>
+            <div className='flex items-center '>
+                <img className='h-[200px]' src={loading} alt='Loading'/>
             </div>
         </div>
     )
