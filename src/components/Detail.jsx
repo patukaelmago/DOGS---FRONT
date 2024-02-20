f45f179ba20d8e2fd192d4b14da05c6b94ded8d5
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-20 04:28:03 -0300

    detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -3,17 +3,16 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetail } from '../redux/actions/index';
 import { useEffect } from "react";
-import '../styles/Detail.css';
 import Loading from "./Loading";
+import Footer from './Footer';
 
 export default function Detail(props){
     const dispatch = useDispatch();
     const history=useHistory();
     const myDog = useSelector((state) => state.detail)
-
+   
     useEffect(() => {
         dispatch(getDetail(props.match.params.id));
-        
     },[dispatch])
 
     function handleHistory(){
@@ -21,28 +20,34 @@ export default function Detail(props){
     }
 
     return (
-        <div className="detail-container">
+        <>
+        <div className="pb-4 pt-8 h-screen">
             {
                 myDog.length?
                 <>
                 <div className='detail-card'>
-                    <div top-row background-top-row>
-                    <h1 className="top-row background-top-row"> {myDog[0].name}</h1>
+                    <div className="mb-2">
+                    <h1 className="text-black text-2xl font-extrabold pb-6"> {myDog[0].name}</h1>
                     </div>
-                    <img src={myDog[0].image} alt={myDog[0].name} ></img>
-                    <h2 className="temp">{myDog[0].createdInDb === true ? myDog[0].temperaments?.map(t => t.name +"/ ") :  myDog[0].temperaments?.join(', ')}</h2>
+                    <div className="flex justify-center">
+                    <img className="pb-4 h-[500px]" src={myDog[0].image} alt={myDog[0].name} ></img>
+                    </div>
+                    <div className="pb-8">
+                    <h2 className="font-extrabold mx-4 pt-4">{myDog[0].createdInDb === true ? myDog[0].temperaments?.map(t => t.name +"/ ") :  myDog[0].temperaments?.join(', ')}</h2>
                     <br/>
-                    <h3>Weight:     {myDog[0].weightMin} - {myDog[0].weightMax}</h3>
-                    <h3>Height:     {myDog[0].heightMin} - {myDog[0].heightMax}</h3>
-                    <h3>Lifespan:     {myDog[0].lifespan}</h3>
-                    
+                    <h3 className="font-bold">Weight:     {myDog[0].weightMin} - {myDog[0].weightMax}</h3>
+                    <h3 className="font-bold">Height:     {myDog[0].heightMin} - {myDog[0].heightMax}</h3>
+                    <h3 className="font-bold">Lifespan:     {myDog[0].lifespan}</h3>
+                    </div>
                 </div>
-                <div className="back-container-d">
-                <button onClick={handleHistory} className="back-d">Back</button>
+                <div className="">
+                <button onClick={handleHistory} className="border px-3 text-xl py-1 rounded-xl  font-bold">Back</button>
                 </div>
                 </>
                 : <Loading/>
             }
+           <Footer />
         </div>
+        </>
     )
 }
