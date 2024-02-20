20494a5666dd2f6f7942ffc3f643de862e5426da
Patricio Uskaer <patriciouskaer@gmail.com>
2024-02-20 04:29:07 -0300

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Card.css';
 
-export default function Card ({image, name, temperaments, weightMin, weightMax, id}){
+export default function Card ({image, name, id}){
   
     return (
-      <div className='card-container'>
+      <div className='my-10'>
         <Link to={`/detail/${id}`}>
-          <div className='card'>
-            <div className='background-top-row'>
-              <h2 className='info'>{name}</h2>
+          <div className=''>
+            <div className='mb-2'>
+              <h2 className='text-black text-2xl font-extrabold'>{name}</h2>
             </div>
-            <div className='temps'>
+            {/* <div className='temps'>
               {temperaments && typeof temperaments[0] === 'object'? temperaments?.map(t=>(
               t.name + ', '
               )):temperaments?.join(', ')}
-            </div >
-            <div className="image-container">
-                <img className="imagenenen" src={image} alt={`${name}`} height= '250px' width='200px'/>
+            </div > */}
+            <div className="">
+                <img className="h-[156px] w-[180px] rounded-3xl mx-auto" src={image} alt={`${name}`} />
             </div>
-            <div className='weight'>
+            {/* <div className='weight'>
               <h4 className='info'>Min Weight: {weightMin} kg</h4>
               <h4 className='info'>Max Weight: {weightMax} kg</h4>
-            </div>
+            </div> */}
           </div>
         </Link>
       </div>
